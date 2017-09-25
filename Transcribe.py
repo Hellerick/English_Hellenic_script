0dc74010a0ba298dec72148c8bae63294ab0664e
Hellerick <hellerickferlibay@gmail.com>
2017-09-25 17:08:03 +0700

    From SSP

--- Transcribe.py
+++ Transcribe.py
@@ -7,60 +7,47 @@ import pickle
 # https://ru.wikipedia.org/wiki/%D0%90%D0%BD%D0%B3%D0%BB%D0%BE-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B0%D1%8F_%D0%BF%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F_%D1%82%D1%80%D0%B0%D0%BD%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%86%D0%B8%D1%8F
 
 project_path = {
-    'DESKTOP-62BVD4A': 'd:\KPV\Github\English_Cyrillic_script',
-    'hellerick-C17A': r'/home/hellerick/PycharmProjects/English_Cyrillic_script',
-    'Superkomp': r'D:\HCF\PyCharmProjects\English_Cyrillic_script',
+    'DESKTOP-62BVD4A': 'd:\KPV\Github\English_Hellenic_script',
+    'hellerick-C17A': r'/home/hellerick/PycharmProjects/English_Hellenic_script',
+    'Superkomp': r'D:\HCF\PyCharmProjects\English_Hellenic_script',
 }[platform.node()]
 
-Hellerick_2015 = 'H15'
-Schoolhouse = 'SCH'
+Grecocoptic_2017 = 'GC7'
 
-system = 'H15'
+system = Grecocoptic_2017
 
 English_alphabet = 'abcdefghijklmnopqrstuvwxyzáâéëíïúſ'
 
 hyphenate = True
 
 
-def translit_cyr(word):
-    if system == 'H15':
+def translit_grc(word):
+    if system == 'GC7':
         translit_pairs=[
-            ['ce', 'се'], ['ci', 'сі'], ['cy', 'сі'], ['ya', 'іа'], ['ye', 'іе'],
-            ['yi', 'іі'], ['yo', 'іо'], ['yu', 'ю'], ['ch', 'ч'],
-            ['sh', 'ш'], ['th', 'ѳ'],
-            ['a', 'а'], ['b', 'б'], ['c', 'к'],
-            ['d', 'д'], ['e', 'е'], ['f', 'ф'], ['g', 'г'], ['h', 'х'], ['i', 'і'],
-            ['j', 'џ'], ['k', 'к'], ['l', 'л'], ['m', 'м'], ['n', 'н'],
-            ['o', 'о'], ['p', 'п'], ['q', 'к'], ['r', 'р'], ['s', 'с'], ['t', 'т'],
-            ['u', 'у'], ['v', 'в'], ['w', 'ѵ'], ['x', 'кс'], ['y', 'і'], ['z', 'з'],
-            ['á', 'а́']
-        ]
-    elif system == 'HCF':
-        translit_pairs=[
-            ['ce', 'се'], ['ci', 'си'], ['cy', 'си'], ['ya', 'ья'], ['ye', 'ье'],
-            ['yi', 'ьи'], ['yo', 'ьо'], ['yu', 'ью'], ['ch', 'ч'],
-            ['sh', 'ш'], ['th', 'т'],
-            ['yá', 'ья́'],
-            ['a', 'а'], ['b', 'б'], ['c', 'к'],
-            ['d', 'д'], ['e', 'е'], ['f', 'ф'], ['g', 'г'], ['h', 'х'], ['i', 'и'],
-            ['j', 'дж'], ['k', 'к'], ['l', 'л'], ['m', 'м'], ['n', 'н'],
-            ['o', 'о'], ['p', 'п'], ['q', 'к'], ['r', 'р'], ['s', 'с'], ['t', 'т'],
-            ['u', 'у'], ['v', 'в'], ['w', 'ў'], ['x', 'кс'], ['y', 'и'], ['z', 'з'],
-            ['á', 'а́']
+            ['ce', 'ϡε'], ['ci', 'ϡι'], ['cy', 'ϡι'], ['ya', 'ια'], ['ye', 'ιε'],
+            ['yi', 'ιι'], ['yo', 'ιο'], ['yu', 'ιυ'], ['ch', 'ϭ'],
+            ['sh', 'ϭ'], ['th', 'θ'],
+            ['a', 'α'], ['b', 'β'], ['c', 'κ'],
+            ['d', 'δ'], ['e', 'ε'], ['f', 'φ'], ['g', 'γ'], ['h', '῾'], ['i', 'ι'],
+            ['j', 'ϫ'], ['k', 'κ'], ['l', 'λ'], ['m', 'μ'], ['n', 'ν'],
+            ['o', 'ο'], ['p', 'π'], ['q', 'κ'], ['r', 'ρ'], ['s', 'σ'], ['t', 'τ'],
+            ['u', 'υ'], ['v', 'ϝ'], ['w', 'ϝ'], ['x', 'ξ'], ['y', 'ι'], ['z', 'ζ'],
+            ['á', 'ά']
         ]
     for pair in translit_pairs:
         word = word.replace(*pair)
-    word = re.sub(r'(\A|[аеиоуяю])ь', r'\1', word)
-    word = re.sub(r'(\A|[аеиоуяю])е', r'\1э', word)
+    word = re.sub(r'σ\b', r'ς', word)
+    for a,b in zip('αεηιοόωῳ', 'ἁἑἡἱὁὅὑὡᾡ'):
+        word = re.sub('῾'+a, b, word)
     return word
 
 
-def try_lat_phonet_matching(prev_cyr, next_lat, next_phonet, rules):
+def try_lat_phonet_matching(prev_grc, next_lat, next_phonet, rules):
     global watch
-    watch += f'\n - {prev_cyr}, {next_lat}, {repr(next_phonet)}'
+    watch += f'\n - {prev_grc}, {next_lat}, {repr(next_phonet)}'
     if next_lat=='' and next_phonet==[]:
-        # print(f' : {prev_cyr}')
-        return prev_cyr
+        # print(f' : {prev_grc}')
+        return prev_grc
     else:
         for rule in rules:
             if re.match(rule[0], next_lat) and re.match(rule[1], ' '.join(next_phonet)):
@@ -70,26 +57,28 @@ def try_lat_phonet_matching(prev_cyr, next_lat, next_phonet, rules):
                 else:
                     phonet_len = len(re.match(rule[1], ' '.join(next_phonet)).group().split())
                 attempt = try_lat_phonet_matching(
-                    prev_cyr+rule[2],
+                    prev_grc+rule[2],
                     next_lat[re.match(rule[0], next_lat).span()[1]:],
                     next_phonet[phonet_len:],
                     rules
                 )
-                if attempt: return attempt
+                if attempt:
+                    # watch += '\n'+repr(rule)
+                    return attempt
 
 
-def phonet_cyr(lat, phonet, rules=[]):
+def phonet_grc(lat, phonet, rules=[]):
     global watch
     watch = f'{lat} [{phonet}]'
-    cyr = try_lat_phonet_matching(prev_cyr='', next_lat=lat, next_phonet=phonet.split(), rules=rules)
+    cyr = try_lat_phonet_matching(prev_grc='', next_lat=lat, next_phonet=phonet.split(), rules=rules)
     if cyr == None:
         print(watch)
         raise Exception('Not matched!')
-    # print('phonet_cyr', cyr)
+    # print('phonet_grc', cyr)
     return cyr
 
 
-def convert_word(word, cyr_dict):
+def convert_word(word, grc_dict):
     if word == '':
         return word
     if word[0].isupper():
@@ -99,7 +88,7 @@ def convert_word(word, cyr_dict):
             case = 'allcaps'
     else:
         case = 'lower'
-    word = cyr_dict[word.lower()]
+    word = grc_dict[word.lower()]
     if case == 'title':
         word = word[0].upper() + word[1:]
     elif case == 'allcaps':
@@ -108,62 +97,43 @@ def convert_word(word, cyr_dict):
     return word
 
 
-def convert_text(text, cyr_dict):
+def convert_text(text, grc_dict):
     text = re.sub(r'(['+English_alphabet+r',]\s)I\b', r'\1i', text)
     text = re.sub(r'(['+English_alphabet+r',]\s?—\s?)I\b', r'\1i', text)
     text = re.split('(['+English_alphabet+English_alphabet.upper()+'’]*['+English_alphabet+English_alphabet.upper()+'][0-9]?)', text)
     #print(f'Text <{text}>')
     for n, word in enumerate(text):
         if n%2 == 1:
-            text[n] = convert_word(word, cyr_dict)
+            text[n] = convert_word(word, grc_dict)
     return ''.join(text)
 
 
-def try_breaking(word, cyr_dict, phonet_dict, rules):
+def try_breaking(word, grc_dict, phonet_dict, rules):
     boundary = sorted(list(range(1, len(word))), key = lambda x: abs(x - len(word)/2))
     for b in boundary:
-        if (word[:b] in cyr_dict or word[:b] in phonet_dict) and (word[b:] in cyr_dict or word[b:] in phonet_dict):
+        if (word[:b] in grc_dict or word[:b] in phonet_dict) and (word[b:] in grc_dict or word[b:] in phonet_dict):
             print(f'Word broken: {word[:b]} + {word[b:]}')
-            returned = [cyr_dict[w] if w in cyr_dict else phonet_cyr(w, phonet_dict[w], rules) for w in [word[:b], word[b:]]]
+            returned = [grc_dict[w] if w in grc_dict else phonet_grc(w, phonet_dict[w], rules) for w in [word[:b], word[b:]]]
             return '¬'.join(returned)
 
 
-def postprocess(cyr_dict, user_dict, full_normalization = False):
-    lat_words = cyr_dict.keys()
-    cyr_words = '\n'.join(cyr_dict.values()) # т̈
-    rules = [
-        [r'(\b|-|а|е|о|у|ў|ю|́)е', r'\1э̀'],
-        [r'(\b|-|а|е|о|у|ў|ю|́)ё', r'\1ӭ'],
-        [r'ьйа','ья'],
-        [r'ьйу', 'ью'],
-        [r'\bьйе', 'йе'],
-        [r'ьйе', 'ье'],
-        [r'(\b|а|е|и|о|у|ю|́)ь', r'\1'],
-        ['э̀й', 'эй'],
-        [r'([бгдзлмнпрстф])\1([бдзклмнпртсф])', r'\1\2'],
-        [r'([гзклмрстф])\1(\b|ь)', r'\1\2'],
-        [r'ўу', 'в̆у'],
-    ]
-    if full_normalization:
-        rules = rules + [
-            ['̆в', 'в'],
-            ['ў', 'у'],
-            ['(т̈|д̈|ҙ|ҫ)', 'т'],
-            ['[̈́̀]', ''],
-        ]
-    for rule in rules:
-        cyr_words = re.sub(rule[0], rule[1], cyr_words)
-    cyr_dict = dict(zip(lat_words, cyr_words.split('\n')))
+def postprocess(grc_dict, user_dict, full_normalization = False):
+    lat_words = grc_dict.keys()
+    grc_words = '\n'.join(grc_dict.values()) # т̈
+    grc_words = re.sub(r'σ\b', r'ς', grc_words)
+    for a,b in zip('αεηιοόωῳ', 'ἁἑἡἱὁὅὑὡᾡ'):
+        grc_words = re.sub('῾'+a, b, grc_words)
+    grc_dict = dict(zip(lat_words, grc_words.split('\n')))
     for word in user_dict:
-        cyr_dict[word] = user_dict[word]
-    return cyr_dict
+        grc_dict[word] = user_dict[word]
+    return grc_dict
 
 
-def dialog_mode(cyr_dict):
+def dialog_mode(grc_dict):
     while True:
         u = input()
         try:
-            print(cyr_dict[u.lower()])
+            print(grc_dict[u.lower()])
         except KeyError:
             pass
         if u == '':
@@ -247,32 +217,32 @@ def make_local_dictionary(file_path, word_list):
         if not word in phonet_dict:
             phonet_dict[word] = UK_dict[word]
 
-    cyr_dict = {word:user_dict[word] for word in user_dict}
+    grc_dict = {word:user_dict[word] for word in user_dict}
     for word in word_list:
         if not word in user_dict and word in phonet_dict:
-            cyr_dict[word] = phonet_cyr(word, phonet_dict[word], rules)
+            grc_dict[word] = phonet_grc(word, phonet_dict[word], rules)
     for word in word_list:
-        if not word in cyr_dict:
-            attempt = try_breaking(word, cyr_dict, phonet_dict, rules)
+        if not word in grc_dict:
+            attempt = try_breaking(word, grc_dict, phonet_dict, rules)
             if attempt:
-                cyr_dict[word] = attempt
+                grc_dict[word] = attempt
             else:
                 print('Not found:', word)
-                cyr_dict[word] = translit_cyr(word)
-    if system=='SCH':
-        cyr_dict = postprocess(cyr_dict, user_dict, full_normalization=False)
-    cyr_dict = eval(hyphenate_code(repr(cyr_dict)))
-    # print('make_local_dictionary', cyr_dict)
+                grc_dict[word] = translit_grc(word)
+    if system=='GC7':
+        grc_dict = postprocess(grc_dict, user_dict, full_normalization=False)
+    grc_dict = eval(hyphenate_code(repr(grc_dict)))
+    # print('make_local_dictionary', grc_dict)
 
-    cyr_list = sorted([word+' = '+cyr_dict[word] for word in cyr_dict])
-    cyr_list = '\n'.join(cyr_list)
+    grc_list = sorted([word+' = '+grc_dict[word] for word in grc_dict])
+    grc_list = '\n'.join(grc_list)
     with open(local_dict_path, mode='wt', encoding='utf8') as f:
-        f.write(cyr_list)
+        f.write(grc_list)
 
     if not 'default.txt' in file_path:
-        pickle.dump(cyr_dict, open(local_dict_pickle_path, mode='wb'))
+        pickle.dump(grc_dict, open(local_dict_pickle_path, mode='wb'))
 
-    return cyr_dict
+    return grc_dict
 
 
 def hyphenate_code(code):
@@ -334,10 +304,10 @@ def convert_code(code, file_path=os.path.join(project_path, 'default.txt')):
     word_list_path = re.sub(r'\.[a-z]+\Z', '.WordList.txt', file_path)
     with open(word_list_path, 'wt', encoding='utf8') as word_list_file:
         word_list_file.write('\n'.join(word_list))
-    cyr_dict = make_local_dictionary(file_path, word_list)
+    grc_dict = make_local_dictionary(file_path, word_list)
     for n, text in enumerate(code):
         if n%2 == 0:
-            code[n] = convert_text(text, cyr_dict)
+            code[n] = convert_text(text, grc_dict)
     code = ''.join(code)
     code = re.sub(r'<!--\(--([^>]+)--\)-->', r'\1', code)
     return code
@@ -354,17 +324,16 @@ def convert_file(file_path):
 
 def main():
 
-    file_path = os.path.join(project_path, 'Examples', 'Eoin Colfer. Artemis Fowl 01.html')
-    file_path = r'D:\HCF\Dropbox\Job-shared\Lib\Vonnegut, Kurt\Kurt Vonnegut Welcome to the Monkey House Stories.html'
-    file_path = r'/home/hellerick/Dropbox/Lib/Fiction/Stewart, Mary/Stewart, Mary - The Little Broomstick.code.txt'
-    file_path = r"D:\HCF\Dropbox\Job-shared\Lib\Stewart, Mary\Stewart, Mary - The Little Broomstick.code.txt"
+    # file_path = os.path.join(project_path, 'Examples', 'Eoin Colfer. Artemis Fowl 01.html')
+    file_path = r"D:\HCF\Dropbox\Job-shared\Lib\Ransome, Arthur\Ransome - Swallows and Amazons.fodt"
+    file_path = r"D:\HCF\PyCharmProjects\English_Hellenic_script\Examples\About the Greek alphabet.txt"
 
     if True:
         convert_file(file_path)
 
     if True:
         print(convert_code('''
-    carbon-fibre
+    derived
     '''))
 
     # print(convert_code('''sometimes'''))
